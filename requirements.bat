15204cda64c2445b477f70d1fd5649796735c17d
Empty <77683874+MrX0955@users.noreply.github.com>
2023-02-24 17:30:11 +0300

    Update requirements.bat

--- requirements.bat
+++ requirements.bat
@@ -1,3 +1,27 @@
-start cracked.io/Sweety666
 pip install cloudscraper
-pause
+color 0a
+@echo off
+cls
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+echo "Vouch me please"
+timeout 5
+start https://cracked.io/Sweety666
